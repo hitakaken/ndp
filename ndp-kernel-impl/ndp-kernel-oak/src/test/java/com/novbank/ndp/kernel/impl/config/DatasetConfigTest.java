f8a063858f9ed40a35d4909010f1fdc70dbe2b18
hitakaken <hitakaken@gmail.com>
2015-05-21 17:59:13 +0800

    Initial commit

--- ndp-kernel-impl/ndp-kernel-oak/src/test/java/com/novbank/ndp/kernel/impl/config/DatasetConfigTest.java
+++ ndp-kernel-impl/ndp-kernel-oak/src/test/java/com/novbank/ndp/kernel/impl/config/DatasetConfigTest.java
@@ -1,20 +1,16 @@
 package com.novbank.ndp.kernel.impl.config;
 
-import com.novbank.ndp.kernel.impl.server.HdfsApp;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.security.UserGroupInformation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.data.hadoop.store.dataset.DatasetOperations;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.hadoop.fs.HdfsResourceLoader;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.security.PrivilegedExceptionAction;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
@@ -24,14 +20,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:/hadoop-context.xml")
+@TestPropertySource(properties = { "spring.hadoop.fsUri=hdfs://SHLIB-534:9000"})
 public class DatasetConfigTest {
     @Autowired
     private org.apache.hadoop.conf.Configuration conf;
 
+    @Autowired
+    private FileSystem hdfs;
+
+    @Autowired
+    @Qualifier("loader")
+    private HdfsResourceLoader loader;
+
     @Test
     public void testDataStoreWriter() throws Exception {
         //conf.set("fs.default.name","hdfs://SHLIB-534:9000");
-        UserGroupInformation ugi = UserGroupInformation.createProxyUser("kcao", UserGroupInformation.getLoginUser());
+        /*UserGroupInformation ugi = UserGroupInformation.createProxyUser("kcao", UserGroupInformation.getLoginUser());
 
         ugi.doAs((PrivilegedExceptionAction) () -> {
             org.apache.hadoop.conf.Configuration jobconf = new org.apache.hadoop.conf.Configuration();
@@ -40,9 +44,17 @@ public class DatasetConfigTest {
             FileStatus[] files = fs.listStatus(new Path("/home"));
             Arrays.asList(files).forEach(f -> System.out.println(f.getPath()));
             return null;
-        });
-        FileSystem fs = FileSystem.get(conf);
+        });*/
+        //FileSystem fs = FileSystem.get(conf);
+        /*FileSystem fs = FileSystem.get(FileSystem.getDefaultUri(conf),conf,"xtwh");
         FileStatus[] files = fs.listStatus(new Path("/"));
         Arrays.asList(files).forEach(f -> System.out.println(f.getPath()));
+        for (FileStatus file : files) {
+
+        }*/
+        Arrays.asList(hdfs.listStatus(new Path("/"))).forEach(f -> System.out.println(f.getPath()));
+        System.out.println("");
+        Arrays.asList(loader.getResources("/*")).forEach(r -> System.out.println(r.getFilename()));
+
     }
 }
