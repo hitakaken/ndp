528c4b2203c43b04284663a6c7975b0c41d7f157
hitakaken <hitakaken@gmail.com>
2015-06-01 17:50:34 +0800

    Initial commit

--- ndp-core/src/main/java/com/novbank/ndp/core/helper/reflect/ProxyPropertyClass.java
+++ ndp-core/src/main/java/com/novbank/ndp/core/helper/reflect/ProxyPropertyClass.java
@@ -1,5 +1,12 @@
 package com.novbank.ndp.core.helper.reflect;
 
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by hp on 2015/6/1.
  */
@@ -9,6 +16,22 @@ public class ProxyPropertyClass<T> {
 
     public ProxyPropertyClass(Class pojoClass) {
         this.pojoClass = pojoClass;
+        initialize();
+    }
+
+    private void initialize(){
+        Map<String,Method> getters = new HashMap<>();
+        Map<String,Method> setters = new HashMap<>();
+        try {
+            for (PropertyDescriptor pd : Introspector.getBeanInfo(pojoClass).getPropertyDescriptors()) {
+                if (pd.getReadMethod() != null && !"class".equals(pd.getName()))
+                    getters.put(pd.getName(), pd.getReadMethod());
+                if (pd.getWriteMethod() != null && !"class".equals(pd.getName()))
+                    setters.put(pd.getName(), pd.getWriteMethod());
+            }
+        } catch (IntrospectionException e) {
+            //ignore;
+        }
     }
 
     public T newInstance(){
