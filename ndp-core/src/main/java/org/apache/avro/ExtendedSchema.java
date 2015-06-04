45c408c808e049940cfce90e412e407f4b77e0a2
Ken <kcao@libnet.sh.cn>
2015-06-04 22:12:54 +0800

    Initial commit

---
+++ ndp-core/src/main/java/org/apache/avro/ExtendedSchema.java
@@ -0,0 +1,8 @@
+package org.apache.avro;
+
+/**
+ * Created by CaoKe on 2015/6/4.
+ */
+public class ExtendedSchema {
+
+}
--- ndp-core/src/main/java/org/apache/avro/RecordSchema.java
+++
@@ -1,7 +0,0 @@
-package org.apache.avro;
-
-/**
- * Created by hp on 2015/6/3.
- */
-public class RecordSchema {
-}
